6d90bffbdc4748c31d750c84b34ce7487a310b17
Brandon Brasson <rocksong622@gmail.com>
2017-09-25 21:01:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
 var playlist = {
    "nykki heaton" : "bad intentions"
  }
- 
+
  var updatePlaylist = (playlist, artist, song) => {
    playlist[artist] = song
    return playlist
  }
- 
+
  var removeFromPlaylist = (playlist, artist) => {
    delete playlist[artist]
    return playlist
